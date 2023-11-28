b2957bd4a4cea346326c5ea0a4f514c7212503b2
yk-graph <yokokura.tatsuya@gmail.com>
2023-11-28 14:03:53 +0900

    01_Environment_setup

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+
+// [Tips] フォントを指定方法
+const font = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
